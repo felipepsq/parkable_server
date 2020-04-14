1736d032c662dacd87dedac79ae2512cc6291595
felipepsq <felipe.psq@outlook.com>
2020-04-13 21:52:54 -0300

    test

--- server/server.js
+++ server/server.js
@@ -58,6 +58,7 @@ const setDefaultMarkers = () => {
 }
 
 const setCron = (markerID, userDate) => {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAA")
 
     var rule = new schedule.RecurrenceRule();
     rule.date = userDate.getDate()
