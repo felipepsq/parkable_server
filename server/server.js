c6c797fd2091b8fbb5e5f4e971c2b80e543671f1
felipepsq <felipe.psq@outlook.com>
2020-04-19 21:27:13 -0300

    woke dyno changes

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.listen(port, () => {
     console.log(`Listening on port ${port}!`)
     wakeDyno({
         url: 'https://git.heroku.com/parkable-server.git',
-        // interval: 60000, // interval in milliseconds (1 minute in this example)
+        interval: 1200000 // interval in milliseconds (20 minutes in this example)
         // startNap: [5, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
         // endNap: [9, 59, 59, 999] // time to wake up again, in UTC (09:59:59.999 in this example)
     }).start();
