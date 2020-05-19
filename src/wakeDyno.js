3842e22ba3b80c7e1dd35716bbfb048d494741bc
felipepsq <felipe.psq@outlook.com>
2020-05-19 13:14:58 -0300

    dyno changes

--- src/wakeDyno.js
+++ src/wakeDyno.js
@@ -2,9 +2,9 @@ const wakeDyno = require("woke-dyno");
 
 export const wakeUpDyno = () => {
     wakeDyno({
-        url: 'https://parkable-server.herokuapp.com/',  // url string
-        interval: 900000, // interval in milliseconds
-        // startNap: [5, 0, 0, 0], // the time to start nap in UTC, as [h, m, s, ms] (05:00 UTC in this example)
-        // endNap: [9, 59, 59, 999] // time to wake up again, in UTC (09:59:59.999 in this example)
+        url: 'https://parkable-server.herokuapp.com/',
+        interval: 900000,
+        // startNap: [5, 0, 0, 0],
+        // endNap: [9, 59, 59, 999]
     }).start()
 }
